1d97299c8b22f3d5f00b9b4ba76f59131f52bcca
mayota <m.yota0906kapa@gmail.com>
2026-06-17 21:58:39 +0900

    modify variable name

--- 49-Group-Anagrams/step2.py
+++ 49-Group-Anagrams/step2.py
@@ -1,11 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        sorted_to_originals: dict[str, list[str]] = {}
+        grouped_anagrams: dict[str, list[str]] = {}
         for s in strs:
             sorted_s = str(sorted(s))
-            if sorted_s in sorted_to_originals:
-                sorted_to_originals[sorted_s].append(s)
+            if sorted_s in grouped_anagrams:
+                grouped_anagrams[sorted_s].append(s)
             else:
-                sorted_to_originals[sorted_s] = [s]
+                grouped_anagrams[sorted_s] = [s]
 
-        return list(sorted_to_originals.values())
+        return list(grouped_anagrams.values())
