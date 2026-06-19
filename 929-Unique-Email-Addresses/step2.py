91f4ab65064d1a2078e818e7bccee9b2a3b37965
mayota <m.yota0906kapa@gmail.com>
2026-06-19 23:26:42 +0900

    step2 その2

--- 929-Unique-Email-Addresses/step2.py
+++ 929-Unique-Email-Addresses/step2.py
@@ -1,21 +1,11 @@
 class Solution:
-    def normalize(self, email: str) -> str:
-        local_name = ""
-        index = 0
-        after_plus = False
-        while email[index] != "@":
-            if not after_plus:
-                if email[index] == "+":
-                    after_plus = True
-                elif email[index] != ".":
-                    local_name += email[index]
-            index += 1
-
-        return local_name + email[index:]
-
     def numUniqueEmails(self, emails: list[str]) -> int:
         unique_emails = set()
         for email in emails:
-            normalized_email = self.normalize(email)
-            unique_emails.add(normalized_email)
+            at_split = email.split("@")
+            domain_name = at_split[1]
+            local_name = at_split[0]
+            normalized_local_name = local_name.split("@")[0].replace(".", "")
+            unique_emails.add(normalized_local_name + "@" + domain_name)
+
         return len(unique_emails)
