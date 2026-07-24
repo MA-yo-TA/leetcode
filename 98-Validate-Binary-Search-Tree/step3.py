c07516976aa464b02840887975a9a56a2dd40488
Yota Maeda <yota.maeda@dena.com>
2026-07-24 23:33:56 +0900

    step2,3

---
+++ 98-Validate-Binary-Search-Tree/step3.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        node_and_bound = [(root, float("-inf"), float("inf"))]
+        while node_and_bound:
+            node, lower, upper = node_and_bound.pop()
+            if node is None:
+                continue
+
+            if not (lower < node.val < upper):
+                return False
+
+            node_and_bound.append((node.left, lower, node.val))
+            node_and_bound.append((node.right, node.val, upper))
+
+        return True
